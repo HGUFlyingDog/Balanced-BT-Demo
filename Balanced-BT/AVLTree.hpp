b067a5035b73b14372ef52433a5ba19a593fef59
HGUFlyingDog <suidonghui666@gmail.com>
2025-06-22 23:28:57 +0800

    chore(AVLTree):简单维护 发现不太适合写函数重载，已经删除 给 一些函数后边增加了const

--- Balanced-BT/AVLTree.hpp
+++ Balanced-BT/AVLTree.hpp
@@ -4,28 +4,28 @@
 
 class AVLTree
 {
+
 public:
 	AVLTree() {};
-	~AVLTree() 
+	~AVLTree()
 	{
 		clear(_root);
 	};
 
-
-	void printInOrder(const Node* root)
+	void printInOrder(const Node* root) const
 	{
-		if (root == nullptr) return ;
+		if (root == nullptr) return;
 		printInOrder(root->_left);
 		cout << root->_key << " ";
 		printInOrder(root->_right);
 	}
 
-	void print()
+	void print() const
 	{
 		printInOrder(_root);
 		cout << endl;
 	}
-	 
+
 	//插入一个值
 	void insert(int key)
 	{
@@ -49,14 +49,14 @@ public:
 			{
 				prev = cur;
 				cur = cur->_right;
-
 			}
 			else
 			{
-				cout << "插入" << key << "失败，因为有重复的节点"<<endl;
+				cout << "插入" << key << "失败，因为有重复的节点" << endl;
 				return;
 			}
 		}
+
 		cur = new Node(key);
 		if (key < prev->_key)
 		{
@@ -66,11 +66,11 @@ public:
 		{
 			prev->_right = cur;
 		}
-		
+
 		this->print();
 	}
 
-	
+
 
 private:
 	Node* _root = nullptr; // 根节点指针
@@ -81,13 +81,6 @@ private:
 		clear(root->_left);
 		clear(root->_right);
 		delete root;
-
 	}
 };
 
-
-//std::ostream& operator<<(std::ostream& out, const AVLTree& Tree)//写个运算符重载函数练习一下
-//{
-//	tree.print(out);
-//	return out;
-//}
