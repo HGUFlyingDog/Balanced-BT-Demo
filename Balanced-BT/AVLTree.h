1563c3e644904b8f9055560daa7926e985e00cd0
HGUFlyingDog <suidonghui666@gmail.com>
2025-06-22 20:45:10 +0800

    基本结构完成

---
+++ Balanced-BT/AVLTree.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include"Node.h"
+
+class AVLTree
+{
+public:
+    AVLTree() {};
+    ~AVLTree() {};
+private:
+    Node* _root = nullptr; // 根节点指针
+    
+
+};
---
+++ Balanced-BT/Node.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+using std::cin;
+using std::cout;
+// 在污染全局命名空间和简化代码之间取得平衡
+
+// 先不考虑模板，专注于int类型
+
+
+class Node
+{
+    friend class AVLTree;// 这里使用了友元函数保证AVLTree类可以访问我的Node的私有成员
+public:
+    Node(int key)
+        :_key(key), _left(nullptr), _right(nullptr),_height(1)
+    {
+    }
+
+private:
+    int _key;            // 键
+    Node* _left;         // 左子树
+    Node* _right;        // 右子树
+    int _height;         // 节点高度
+};
+
