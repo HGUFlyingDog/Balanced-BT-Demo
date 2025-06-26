d592ff81e8dfe3008e64cdce4df5a67f1f9e4bb1
SDH <suidonghui666@gmail.com>
2025-06-27 06:17:51 +0800

    feat 增加了删除的功能，增加了合并的功能

--- Balanced-BT/AVLTree.hpp
+++ Balanced-BT/AVLTree.hpp
@@ -1,426 +1,486 @@
 #pragma once
 #include<iostream>
 #include"Node.hpp"
+#include<vector>
 
 class AVLTree
 {
 public:
-	AVLTree() {};
+    AVLTree() {};
 
-	~AVLTree() {};
+    ~AVLTree() {
+        clear();
+    };
 
-	// 递归形式的打印结构，这里没有办法使用this指针传参实现，所以使用print函数来传入根节点，这里使用的是中序遍历，在搜索二叉树就是相当于从小到大打印，方便我在快速判断搜索二叉树的建立是否成功
-	void printInOrder(const Node* root)
-	{
-		if (root == nullptr) return;// 终止条件
-		printInOrder(root->_left);// 递归调用左子树
-		cout << root->_key << " "; // 打印
-		printInOrder(root->_right);// 递归调用右子树
-	}
 
-	//这个就是调用printInOrder的函数，使用this指针然后传入根节点的指针
-	void print()
-	{
-		printInOrder(_root);
-		cout << endl;
-	}
+    void merge(AVLTree& other) {
+        if (other._root == nullptr) return; // 另一棵树为空，无需合并
 
-	//插入一个值，传参输入一个int 类型的键值，然后这个函数就是插入这个键值并且保持这个二叉树的平衡
-	void insert(int key)
-	{
-		cout << "现在插入的节点是：" << key << endl;
-		if (_root == nullptr)//适用于创建节点时候的初始化部分，如果是空的节点就动态开辟一个
-		{
-			_root = new Node(key);
-			return;
-		}
-		// 这里使用一个cur表示当前节点和一个prev表示当前节点的前一个节点方便进行链表的插入
-		// 但是后来随着我对代码理解的深入，我在Node里面增加了一个parent来表示prev，后期把这个代码优化一下变得更加简洁
-		Node* cur = _root;
-		Node* prev = cur;
+        // 1. 将另一棵树的所有节点值提取到数组中
+        std::vector<int> values;
+        extractValues(other._root, values);
 
-		while (cur != nullptr)
-		{
-			if (key < cur->_key)// 向左插入
-			{
-				prev = cur;
-				cur = cur->_left;
-			}
-			else if (key > cur->_key)// 向右插入
-			{
-				prev = cur;
-				cur = cur->_right;
-			}
-			else// 我的二叉搜索树目前不支持相同关键字的插入，虽然非法插入，但是不至于抛异常
-			{
-				cout << "插入" << key << "失败，因为有重复的节点" << endl;
-				return;
-			}
-		}
+        // 2. 清空另一棵树
+        other.clear();
 
-		cur = new Node(key);// 这里就是找到了插入的部分，新建节点然后开始插入，调用的是Node的构造函数
-		if (key < prev->_key)
-		{
-			prev->_left = cur;
-		}
-		if (key > prev->_key)
-		{
-			prev->_right = cur;
-		}
+        // 3. 将提取的值插入到当前树中
+        for (int key : values) {
+            insert(key);
+        }
+    }
+    // 打印树结构（中序遍历）
+    void printInOrder(const Node* root)
+    {
+        if (root == nullptr) return;
+        printInOrder(root->_left);
+        cout << root->_key << " ";
+        printInOrder(root->_right);
+    }
 
-		cur->_parent = prev;
-		this->print();//测试一下插入的结果
+    void print()
+    {
+        printInOrder(_root);
+        cout << endl;
+    }
 
-		// 控制平衡
-		// 更新平衡因子
-		// 最坏情况下更新到根节点
-		while (prev)
-		{
-			//这个判断条件的目的就是
-			// 1. 在左子树插入，parent平衡因子++
-			// 2. 在右子树插入，parent平衡因子--
+    // 插入节点
+    void insert(int key)
+    {
+        if (_root == nullptr)
+        {
+            _root = new Node(key);
+            return;
+        }
 
-			if (cur == prev->_left)
-			{
-				prev->_bf++;
-			}
-			else
-			{
-				prev->_bf--;
-			}
+        Node* cur = _root;
+        Node* prev = cur;
 
-			if (prev->_bf == 0)  return;//更新结束 节点已经平衡，不会再往上影响父节点了
-			if (abs(prev->_bf) == 1)
-			{
-				//继续向上更新
-				cur = prev;
-				prev = prev->_parent;
-			}
+        while (cur != nullptr)
+        {
+            prev = cur;
+            if (key < cur->_key)
+            {
+                cur = cur->_left;
+            }
+            else if (key > cur->_key)
+            {
+                cur = cur->_right;
+            }
+            else
+            {
+                cout << "插入" << key << "失败，节点已存在" << endl;
+                return;
+            }
+        }
 
-			else if (abs(prev->_bf) == 2) {
-				if (prev->_bf == 2) {  // 左子树高
-					if (prev->_left->_bf == 1) {  // LL
-						RotateR(prev);
-					}
-					else if (prev->_left->_bf == -1) {  // LR
-						RotateLR(prev);
-					}
-				}
-				else if (prev->_bf == -2) {  // 右子树高
-					if (prev->_right->_bf == -1) {  // RR
-						RotateL(prev);
-					}
-					else if (prev->_right->_bf == 1) {  // RL
-						RotateRL(prev);
-					}
-				}
-				break;
-			}
-			else//平衡因子不等于上面出现的五个数字，直接抛异常，我的程序有大的bug
-			{
-				throw"平衡因子出现问题";
-			}
-		}
-	}
+        cur = new Node(key);
+        cur->_parent = prev;
 
-	// 删除节点并维护平衡
-	bool remove(int key) {
-		// 查找要删除的节点
-		Node* node = findNode(key);
-		if (!node) {
-			cout << "未找到节点: " << key << endl;
-			return false;
-		}
+        if (key < prev->_key)
+        {
+            prev->_left = cur;
+        }
+        else
+        {
+            prev->_right = cur;
+        }
 
-		// 执行删除操作
-		removeNode(node);
-		return true;
-	}
+        // 更新平衡因子
+        Node* parent = prev;
+        while (parent)
+        {
+            if (cur == parent->_left)
+            {
+                parent->_bf++;
+            }
+            else
+            {
+                parent->_bf--;
+            }
 
-	void printTree() {
-		_printTree(_root, 0);
-	}
+            if (parent->_bf == 0)
+            {
+                break;
+            }
+            else if (abs(parent->_bf) == 1)
+            {
+                cur = parent;
+                parent = parent->_parent;
+            }
+            else if (abs(parent->_bf) == 2)
+            {
+                if (parent->_bf == 2)
+                {
+                    if (parent->_left->_bf == 1)
+                    {
+                        RotateR(parent);
+                    }
+                    else
+                    {
+                        RotateLR(parent);
+                    }
+                }
+                else
+                {
+                    if (parent->_right->_bf == -1)
+                    {
+                        RotateL(parent);
+                    }
+                    else
+                    {
+                        RotateRL(parent);
+                    }
+                }
+                break;
+            }
+        }
+    }
 
-	Node* findNode(int key) {
-		Node* cur = _root;
-		while (cur) {
-			if (key < cur->_key) {
-				cur = cur->_left;
-			}
-			else if (key > cur->_key) {
-				cur = cur->_right;
-			}
-			else {
-				return cur;
-			}
-		}
-		return nullptr;
-	}
+    // 删除节点
+    bool remove(int key) {
+        Node* node = findNode(key);
+        if (!node) {
+            cout << "未找到节点: " << key << endl;
+            return false;
+        }
+        removeNode(node);
+        return true;
+    }
+
+    // 打印树结构
+    void printTree() {
+        if (_root == nullptr) {
+            cout << "空树" << endl;
+            return;
+        }
+        _printTree(_root, 0);
+    }
+
+    // 查找节点
+    Node* findNode(int key) {
+        Node* cur = _root;
+        while (cur) {
+            if (key < cur->_key) {
+                cur = cur->_left;
+            }
+            else if (key > cur->_key) {
+                cur = cur->_right;
+            }
+            else {
+                return cur;
+            }
+        }
+        return nullptr;
+    }
+
+    // 清空树
+    void clear() 
+    {
+        _clear(_root);
+        _root = nullptr;
+    }
+
 private:
-	Node* _root = nullptr; // 根节点指针
+    Node* _root = nullptr;
 
-	// 这个就是左旋的操作,要求传入一个Node*的节点实施旋转功能，结果就是部分的结构被旋转了
-	void RotateL(Node* parent)
-	{
-		Node* pparent = parent->_parent;
-		Node* cur = parent->_right;
-		Node* curleft = cur->_left;
+    void extractValues(Node* node, std::vector<int>& values) {
+        if (node == nullptr) return;
+        extractValues(node->_left, values);
+        values.push_back(node->_key);
+        extractValues(node->_right, values);
+    }
 
-		parent->_right = cur->_left;//冲突的左孩子变成右孩子
-		cur->_left = parent;
-		parent->_parent = cur; // 旋转 cur的左孩子变成原来的parent
-		if (curleft != nullptr)
-		{
-			curleft->_parent = parent;// 原来的左孩子的父亲变成parent 加了一个判断防止越界访问
-		}
+    // 左旋
+    void RotateL(Node* parent) {
+        Node* pparent = parent->_parent;
+        Node* cur = parent->_right;
+        Node* curleft = cur->_left;
 
-		if (pparent != nullptr)// 传入节点的父节点指向旋转子树的父节点
-		{
-			if (parent == pparent->_left)
-			{
-				pparent->_left = cur;
-			}
-			else
-			{
-				pparent->_right = cur;
-			}
-			cur->_parent = pparent;// 旋转子树的父节点指向原来的父节点
-		}
+        parent->_right = curleft;
+        if (curleft) {
+            curleft->_parent = parent;
+        }
 
-		else
-		{
-			_root = cur; // 更新根节点
-			cur->_parent = nullptr;
-		}
+        cur->_left = parent;
+        parent->_parent = cur;
 
-		cur->_bf = parent->_bf = 0;
-	}
+        if (pparent) {
+            if (pparent->_left == parent) {
+                pparent->_left = cur;
+            }
+            else {
+                pparent->_right = cur;
+            }
+            cur->_parent = pparent;
+        }
+        else {
+            _root = cur;
+            cur->_parent = nullptr;
+        }
 
-	//这个就是右旋的操作，跟左旋差不多，就是顺序换一下，要求传入一个Node*的节点实施旋转功能，结果就是部分的结构被旋转了
-	void RotateR(Node* parent)
-	{
-		Node* pparent = parent->_parent;
-		Node* cur = parent->_left;
-		Node* curright = cur->_right;
+        // 更新平衡因子
+        parent->_bf = 0;
+        cur->_bf = 0;
+    }
 
-		parent->_left = curright;
-		if (curright) curright->_parent = parent;
+    // 右旋
+    void RotateR(Node* parent) {
+        Node* pparent = parent->_parent;
+        Node* cur = parent->_left;
+        Node* curright = cur->_right;
 
-		cur->_right = parent;
-		cur->_parent = pparent;
-		parent->_parent = cur;
+        parent->_left = curright;
+        if (curright) {
+            curright->_parent = parent;
+        }
 
-		if (pparent) {
-			if (pparent->_left == parent)
-				pparent->_left = cur;
-			else
-				pparent->_right = cur;
-		}
-		else {
-			_root = cur;
-		}
+        cur->_right = parent;
+        parent->_parent = cur;
 
-		cur->_bf = parent->_bf = 0;
-	}
+        if (pparent) {
+            if (pparent->_left == parent) {
+                pparent->_left = cur;
+            }
+            else {
+                pparent->_right = cur;
+            }
+            cur->_parent = pparent;
+        }
+        else {
+            _root = cur;
+            cur->_parent = nullptr;
+        }
 
-	// 操作就是对右旋右孩子然后左旋，要求传入一个Node*的节点实施旋转功能，结果就是这个二叉树已经平衡了
-	void RotateRL(Node* parent)
-	{
-		Node* Rchild = parent->_right;
-		Node* RLchild = Rchild->_left;
+        // 更新平衡因子
+        parent->_bf = 0;
+        cur->_bf = 0;
+    }
 
+    // 先左旋后右旋
+    void RotateLR(Node* parent) {
+        Node* Lchild = parent->_left;
+        Node* LRchild = Lchild->_right;
+        int bf = LRchild->_bf;
 
-		//BUG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		int bf = RLchild->_bf;
-		// 这里会导致bf被修改，会导致一些奇怪的bug
+        // 先左旋左子树
+        RotateL(Lchild);
+        // 再右旋父节点
+        RotateR(parent);
 
-		// 先对右孩子右旋
-		RotateR(Rchild);
-		// 再对parent左旋
-		RotateL(parent);
+        // 更新平衡因子
+        if (bf == 1) {
+            parent->_bf = -1;
+            Lchild->_bf = 0;
+        }
+        else if (bf == -1) {
+            parent->_bf = 0;
+            Lchild->_bf = 1;
+        }
+        else {
+            parent->_bf = 0;
+            Lchild->_bf = 0;
+        }
+        LRchild->_bf = 0;
+    }
 
-		// 旋转后平衡因子的调整
-		// RLchild 旋转前的 _bf 决定了 parent 和 Rchild 的新 _bf
-		if (bf == 1) {
-			parent->_bf = 0;
-			Rchild->_bf = -1;
-		}
-		else if (bf == -1) {
-			parent->_bf = 1;
-			Rchild->_bf = 0;
-		}
-		else { // RLchild->_bf == 0
-			parent->_bf = 0;
-			Rchild->_bf = 0;
-		}
-		RLchild->_bf = 0;
-		parent = RLchild;
-	}
+    // 先右旋后左旋
+    void RotateRL(Node* parent) {
+        Node* Rchild = parent->_right;
+        Node* RLchild = Rchild->_left;
+        int bf = RLchild->_bf;
 
-	// 操作就是对左旋左孩子然后右旋，要求传入一个Node*的节点实施旋转功能，结果就是这个二叉树已经平衡
-	void RotateLR(Node* parent)
-	{
-		Node* Lchild = parent->_left;
-		Node* LRchild = Lchild->_right;
+        // 先右旋右子树
+        RotateR(Rchild);
+        // 再左旋父节点
+        RotateL(parent);
 
-		//BUG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		int bf = LRchild->_bf;
+        // 更新平衡因子
+        if (bf == 1) {
+            parent->_bf = 0;
+            Rchild->_bf = -1;
+        }
+        else if (bf == -1) {
+            parent->_bf = 1;
+            Rchild->_bf = 0;
+        }
+        else {
+            parent->_bf = 0;
+            Rchild->_bf = 0;
+        }
+        RLchild->_bf = 0;
+    }
 
+    // 删除节点核心实现
+    void removeNode(Node* node) {
+        Node* parent = node->_parent;
 
-		// 先对左孩子左旋
-		RotateL(Lchild);
-		// 再对parent右旋
-		RotateR(parent);
+        // 情况1: 叶子节点
+        if (node->_left == nullptr && node->_right == nullptr) {
+            if (parent) {
+                if (parent->_left == node) {
+                    parent->_left = nullptr;
+                    parent->_bf--;
+                }
+                else {
+                    parent->_right = nullptr;
+                    parent->_bf++;
+                }
+            }
+            else {
+                _root = nullptr;
+            }
+            delete node;
+            balanceUpward(parent);
+        }
+        // 情况2: 只有右子树
+        else if (node->_left == nullptr) {
+            Node* child = node->_right;
+            if (parent) {
+                if (parent->_left == node) {
+                    parent->_left = child;
+                    parent->_bf--;
+                }
+                else {
+                    parent->_right = child;
+                    parent->_bf++;
+                }
+                child->_parent = parent;
+            }
+            else {
+                _root = child;
+                child->_parent = nullptr;
+            }
+            delete node;
+            balanceUpward(parent);
+        }
+        // 情况3: 只有左子树
+        else if (node->_right == nullptr) {
+            Node* child = node->_left;
+            if (parent) {
+                if (parent->_left == node) {
+                    parent->_left = child;
+                    parent->_bf--;
+                }
+                else {
+                    parent->_right = child;
+                    parent->_bf++;
+                }
+                child->_parent = parent;
+            }
+            else {
+                _root = child;
+                child->_parent = nullptr;
+            }
+            delete node;
+            balanceUpward(parent);
+        }
+        // 情况4: 有两个子节点
+        else {
+            // 找到后继节点
+            Node* successor = node->_right;
+            while (successor->_left != nullptr) {
+                successor = successor->_left;
+            }
 
+            // 保存后继节点的值和平衡因子
+            int successorKey = successor->_key;
+            int successorBf = successor->_bf;
 
+            // 递归删除后继节点
+            removeNode(successor);
 
-		// 旋转后平衡因子的调整
-		// 根据LRchild的_bf调整parent和Lchild的_bf
-		if (bf == 1) {
-			parent->_bf = 0;
-			Lchild->_bf = -1;
-		}
-		else if (bf == -1) {
-			parent->_bf = 1;
-			Lchild->_bf = 0;
-		}
-		else { // LRchild->_bf == 0
-			parent->_bf = 0;
-			Lchild->_bf = 0;
-		}
-		LRchild->_bf = 0;
-		parent = LRchild;
-	}
+            // 替换当前节点的值
+            node->_key = successorKey;
+        }
+    }
 
-	void removeNode(Node* node)
-	{
-		Node* toDelete = node;
-		Node* parent = node->_parent;
-		int fixDir = 0; // 调整方向：-1(从右子树回溯), 1(从左子树回溯)
+    // 向上平衡
+    void balanceUpward(Node* node) {
+        Node* parent = node;
+        while (parent) {
+            if (abs(parent->_bf) == 1) {
+                // 高度不变，停止回溯
+                break;
+            }
+            else if (parent->_bf == 0) {
+                // 继续向上回溯
+                Node* grandparent = parent->_parent;
+                if (grandparent) {
+                    if (parent == grandparent->_left) {
+                        grandparent->_bf--;
+                    }
+                    else {
+                        grandparent->_bf++;
+                    }
+                }
+                parent = grandparent;
+            }
+            else if (abs(parent->_bf) == 2) {
+                // 需要旋转
+                if (parent->_bf == 2) {
+                    if (parent->_left->_bf >= 0) {
+                        RotateR(parent);
+                    }
+                    else {
+                        RotateLR(parent);
+                    }
+                }
+                else if (parent->_bf == -2) {
+                    if (parent->_right->_bf <= 0) {
+                        RotateL(parent);
+                    }
+                    else {
+                        RotateRL(parent);
+                    }
+                }
 
-		// 情况1: 节点有两个子节点
-		if (node->_left && node->_right) {
-			// 找到后继节点(右子树中最小的节点)
-			Node* succ = node->_right;
-			while (succ->_left) succ = succ->_left;
+                // 旋转后父节点可能改变
+                parent = parent->_parent;
+                if (parent && parent->_bf == 0) {
+                    // 继续向上回溯
+                    Node* grandparent = parent->_parent;
+                    if (grandparent) {
+                        if (parent == grandparent->_left) {
+                            grandparent->_bf--;
+                        }
+                        else {
+                            grandparent->_bf++;
+                        }
+                    }
+                    parent = grandparent;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
 
-			// 保存后继节点的父节点作为调整起点
-			parent = succ->_parent;
+    // 打印树结构（辅助函数）
+    void _printTree(Node* node, int depth) {
+        if (!node) return;
 
-			// 确定调整方向
-			if (succ == succ->_parent->_left) {
-				fixDir = 1; // 从左子树回溯
-			}
-			else {
-				fixDir = -1; // 从右子树回溯
-			}
+        _printTree(node->_right, depth + 1);
 
-			// 用后继节点的值替换当前节点
-			node->_key = succ->_key;
-			toDelete = succ;
-		}
+        for (int i = 0; i < depth; i++)
+            cout << "    ";
+        cout << node->_key << " (bf:" << node->_bf << ")";
 
-		// 情况2/3: 节点只有一个子节点或没有子节点
-		Node* child = toDelete->_left ? toDelete->_left : toDelete->_right;
+        if (node == _root)
+            cout << " [root]";
+        else if (!node->_left && !node->_right)
+            cout << " [leaf]";
 
-		// 更新父节点指针
-		if (parent) {
-			if (toDelete == parent->_left) {
-				parent->_left = child;
-				fixDir = 1; // 从左子树回溯
-			}
-			else {
-				parent->_right = child;
-				fixDir = -1; // 从右子树回溯
-			}
-		}
-		else {
-			// 删除的是根节点
-			_root = child;
-		}
+        cout << endl;
 
-		// 更新子节点的父指针
-		if (child) {
-			child->_parent = parent;
-		}
+        _printTree(node->_left, depth + 1);
+    }
 
-		// 删除节点
-		delete toDelete;
-
-		// 如果树为空，直接返回
-		if (!parent && !child) {
-			_root = nullptr;
-			return;
-		}
-
-		// 回溯调整平衡
-		while (parent) {
-			// 更新平衡因子
-			parent->_bf += (fixDir == 1) ? -1 : 1;
-			if (parent->_parent) {
-				fixDir = (parent == parent->_parent->_left) ? 1 : -1;
-			}
-
-			// 根据平衡因子决定下一步操作
-			if (parent->_bf == 1 || parent->_bf == -1) {
-				// 树高度不变，停止回溯
-				break;
-			}
-			else if (parent->_bf == 0) {
-				// 树高度降低，继续向上回溯
-				fixDir = (parent == parent->_parent->_left) ? 1 : -1;
-				parent = parent->_parent;
-			}
-			else if (abs(parent->_bf) == 2) {
-				// 需要旋转调整
-				Node* temp = parent;
-
-				if (parent->_bf == 2) {
-					if (parent->_left->_bf >= 0) {
-						// LL型
-						RotateR(parent);
-					}
-					else {
-						// LR型
-						RotateLR(parent);
-					}
-				}
-				else if (parent->_bf == -2) {
-					if (parent->_right->_bf <= 0) {
-						// RR型
-						RotateL(parent);
-					}
-					else {
-						// RL型
-						RotateRL(parent);
-					}
-				}
-
-				// 旋转后更新parent指针
-				parent = temp->_parent;
-
-				// 如果旋转后子树高度不变，停止回溯
-				if (parent && parent->_bf != 0) {
-					break;
-				}
-			}
-		}
-	}
-
-	void _printTree(Node* node, int depth)
-	{
-		if (!node) return;
-
-		_printTree(node->_right, depth + 1);
-
-		for (int i = 0; i < depth; i++) cout << "    ";
-		cout << node->_key << " (bf:" << node->_bf << ")";
-
-		if (!node->_left && !node->_right) cout << " [leaf]";
-		else if (node == _root) cout << " [root]";
-
-		cout << endl;
-
-		_printTree(node->_left, depth + 1);
-	}
+    // 清空树（辅助函数）
+    void _clear(Node* node) {
+        if (node == nullptr) return;
+        _clear(node->_left);
+        _clear(node->_right);
+        delete node;
+    }
 };
--- Balanced-BT/main.cpp
+++ Balanced-BT/main.cpp
@@ -1,6 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include"AVLTree.hpp"
 
+
 #include<vector>
+
+#include <Windows.h>
+
+
 // 显示主菜单
 void showMenu() {
     cout << "\n=== AVL树操作菜单 ===\n";
@@ -8,7 +14,9 @@ void showMenu() {
     cout << "2. 删除节点\n";
     cout << "3. 查找节点\n";
     cout << "4. 打印树结构\n";
-    cout << "5. 运行默认测试\n";
+    cout << "5. 打印中序遍历\n";
+    cout << "6. 运行默认测试\n";
+    cout << "7. 合并两棵平衡二叉树\n";
     cout << "0. 退出\n";
     cout << "请选择操作: ";
 }
@@ -17,7 +25,7 @@ void showMenu() {
 void handleInsert(AVLTree& tree) {
     cout << "请输入要插入的整数值: ";
     int key;
-    std::cin >> key;
+    cin >> key;
     tree.insert(key);
     cout << "插入完成，当前树结构:\n";
     tree.printTree();
@@ -27,7 +35,7 @@ void handleInsert(AVLTree& tree) {
 void handleDelete(AVLTree& tree) {
     cout << "请输入要删除的整数值: ";
     int key;
-    std::cin >> key;
+    cin >> key;
     if (tree.remove(key)) {
         cout << "删除成功，当前树结构:\n";
     }
@@ -41,7 +49,7 @@ void handleDelete(AVLTree& tree) {
 void handleSearch(AVLTree& tree) {
     cout << "请输入要查找的整数值: ";
     int key;
-    std::cin >> key;
+    cin >> key;
     bool found = tree.findNode(key) != nullptr;
     cout << (found ? "找到该节点\n" : "未找到该节点\n");
 }
@@ -50,7 +58,7 @@ void handleSearch(AVLTree& tree) {
 void runDefaultTest(AVLTree& tree) {
     cout << "正在运行默认测试用例...\n";
     cout << "插入顺序: ";
-    std::vector<int> test{ 14,9,5,17,11,12,7,19,27,8,18,23 };
+    std::vector<int> test{ 14, 9, 5, 17, 11, 12, 7, 19, 27, 8, 18, 23 };
     for (int val : test) {
         cout << val << " ";
         tree.insert(val);
@@ -60,42 +68,128 @@ void runDefaultTest(AVLTree& tree) {
     tree.printTree();
 }
 
+// 处理树合并操作
+void handleMerge(AVLTree& tree) {
+    AVLTree otherTree;
+    cout << "创建新树进行合并...\n";
+    cout << "请输入要添加到新树的整数值(输入-1结束输入):\n";
+
+    std::vector<int> values;
+    int value;
+    while (true) {
+        cout << "输入值(-1结束): ";
+        cin >> value;
+        if (value == -1) break;
+        values.push_back(value);
+        otherTree.insert(value);
+    }
+
+
+    if (!values.empty()) {
+        cout << "\n新建的树结构:\n";
+        otherTree.printTree();
+
+        cout << "\n正在合并到主树...\n";
+        tree.merge(otherTree);
+
+        cout << "合并完成，当前主树结构:\n";
+        tree.printTree();
+    }
+    else {
+        cout << "未添加任何值，取消合并操作\n";
+    }
+}
+
+void setConsoleColor(int color)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}   
+
+// 恢复默认控制台颜色
+void resetConsoleColor()
+{
+    setConsoleColor(7);
+}
+
+
+void stop() { 
+    char tem;
+    scanf("%c", &tem);
+    setConsoleColor(2);
+    printf("按下任意键继续 ");
+    resetConsoleColor();
+    scanf("%c", &tem);
+    system("cls");
+}
 
 int main()
 {
-	AVLTree tree;
+	//AVLTree tree;
 
-	std::vector<int> test{ 14,9,5,17,11,12,7,19,27,8,18,23 };
+	//std::vector<int> test{ 14,9,5,17,11,12,7,19,27,8,18,23 };
 
-	for (auto ch:test)
-	{
-    		tree.insert(ch);
-	}
+	//for (auto ch:test)
+	//{
+ //   		tree.insert(ch);
+	//}
 
-	tree.printTree();
-	
-	tree.remove(5);
+ //   cout << endl << endl << endl << endl << endl << endl << endl;
 
-	cout << endl << endl << endl << endl << endl << endl << endl;
-	tree.printTree();
+	//tree.printTree();
+	//
+	//tree.remove(5);
+
+	//cout << endl << endl << endl << endl << endl << endl << endl;
+	//tree.printTree();
 
 
-    /*AVLTree tree;
-    int choice = -1;
+    AVLTree tree;
+    int choice = 0;
 
-    while (choice != 0) {
+    cout << "=== AVL平衡二叉树演示程序 ===\n";
+
+
+    do {
         showMenu();
-        std::cin >> choice;
+        cin >> choice;
+
+  
 
         switch (choice) {
-        case 1: handleInsert(tree); break;
-        case 2: handleDelete(tree); break;
-        case 3: handleSearch(tree); break;
-        case 4: tree.printTree(); break;
-        case 5: runDefaultTest(tree); break;
-        case 0: cout << "程序已退出\n"; break;
-        default: cout << "无效选择，请重新输入！\n";
+        case 1:
+            handleInsert(tree);
+            break;
+        case 2:
+            handleDelete(tree);
+            break;
+        case 3:
+            handleSearch(tree);
+            break;
+        case 4:
+            cout << "当前树结构:\n";
+            tree.printTree();
+            break;
+        case 5:
+            cout << "中序遍历结果: ";
+            tree.print();
+            cout << endl;
+            break;
+        case 6:
+            runDefaultTest(tree);
+            break;
+        case 7:
+            handleMerge(tree);
+            break;
+        case 0:
+            cout << "程序已退出，感谢使用！\n";
+            break;
+        default:
+            cout << "无效选择，请重新输入！\n";
         }
-    }*/
+
+  
+        stop();
+    } while (choice != 0);
 	return 0;
 }
